a9360c48bf176e847d0d4a76941608ae2f79406e
Cap2711 <chelsey.penton@keyin.com>
2024-12-02 18:29:04 -0330

    More detail and structure
    added search bar before backend hookup

--- FInalSprint/my-app/src/App.js
+++ FInalSprint/my-app/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="outer-box">
+      <div className="inner-box">
+        <div className="logo">
+          <h1>
+            Naomi & Chels Air
+          </h1>
+          <p>Your Gateway to Seamless Air Travel</p>
+        </div>
+
+
+        <nav className="navbar">
+          <h3><a href='#' target='_blank'>Home</a></h3>
+          <h3><a href='#' target='_blank'>Bookings</a></h3>
+          <h3><a href='#' target='_blank'>Airports</a></h3>
+          <h3><a href='#' target='_blank'>About Us</a></h3>
+          <h3><a href='#' target='_blank'>Contact</a></h3>
+        </nav>
+
+        
+        <div className="welcome">
+          <h1>Welcome, lets find your booking!</h1>
+          <p>Your one-stop solution for managing air travel data!</p>
+        </div>
+
+        
+        <div className="search-section">
+          <div className="search-bar">
+            <input type="text" placeholder="Search..." />
+            <button>Search</button>
+          </div>
+        </div>
+
+       
+        <footer className="footer">
+          <p>&copy; 2024 Air Travel Co. All Rights Reserved.</p>
+        </footer>
+      </div>
     </div>
   );
 }
